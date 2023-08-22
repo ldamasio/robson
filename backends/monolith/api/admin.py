a466e8009028d1084cc392707cec82887af1c1db
ldamasio <ldamasio@gmail.com>
2023-08-22 19:54:22 -0300

    Add multi tenant architecture

--- backends/monolith/api/admin.py
+++ backends/monolith/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Symbol, Order, Operation, Strategy
 
 # Register your models here.
+admin.site.register(Symbol)
+admin.site.register(Order)
+admin.site.register(Operation)
+admin.site.register(Strategy)
--- backends/monolith/api/models.py
+++ backends/monolith/api/models.py
@@ -27,45 +27,5 @@ class Strategy(TenantModel):
     client = models.ForeignKey(Client, blank=True, null=True, on_delete=models.SET_NULL)
     tenant_id='client_id'
 
-# class Client(TenantModel):
-#     tenant_id = 'id'
-#     name = models.CharField(max_length=50)
-#     address = models.CharField(max_length=255)
-#     email = models.CharField(max_length=50, unique=True)
-#     api_url = models.CharField(max_length=255)
-#     stream_url = models.CharField(max_length=255)
-#     access_key = models.CharField(max_length=255)
-#     secret_key = models.CharField(max_length=255)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-# class SymbolManager(TenantManagerMixin, models.Manager):
-#     pass
-
-# class Symbol(TenantModelMixin, models.Model):
-#     client = models.ForeignKey(Client, blank=True, null=True, on_delete=models.SET_NULL)
-#     tenant_id='client_id'
-#     name = models.CharField(max_length=255)
-#     description = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     objects = SymbolManager()
-
-#     class Meta(object):
-#       unique_together = ["id", "client"]
-
-# class OrderManager(TenantManagerMixin, models.Manager):
-#     pass
-
-# class Order(TenantModelMixin, models.Model):
-#     client = models.ForeignKey(Client, blank=True, null=True, on_delete=models.SET_NULL)
-#     tenant_id='client_id'
-#     symbol_orderd = TenantForeignKey(Symbol, blank=True, null=True, on_delete=models.SET_NULL)
-
-#     objects = OrderManager()
-
-# class Operation(TenantModelMixin, models.Model):
-#     client = models.ForeignKey(Client, blank=True, null=True, on_delete=models.SET_NULL)
-#     tenant_id='client_id'
-
-
+    class Meta:
+        verbose_name_plural = 'strategies'
