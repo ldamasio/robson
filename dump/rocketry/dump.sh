8fad2110f250af52f3857d806aae37a6414d74a8
ldamasio <ldamasio@gmail.com>
2023-10-20 10:17:53 -0300

    Ajust initdb script on docker container startup

--- dump/rocketry/dump.sh
+++
@@ -1,3 +0,0 @@
-filepath=/app/$(date +%F_%H-%M-%S).sql
-pg_dump -Fc --host=$PG_HOST --port=$PG_PORT --username=$PG_USER --password=$PG_PASSWORD --dbname=$PG_DBNAME -f filename
-cp $filepath /app/pg-dump.sql
