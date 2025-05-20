97b1a0f3380e8851a299c21ebf77098e658270f2
Leandro Damasio <ldamasio@gmail.com>
2025-05-20 02:27:31 -0300

    feat: Add some configs and rules (#15)
    * Fix rbs-frontend-prod Dockerfile
    
    * Update CI imagem label
    
    * Fix rbs front prod Dockerfile
    
    * Fix rbs front prod Dockerfile
    
    * Update AuthContext to handle error
    
    * Change Nav.Link to LinkContainer on Header
    
    * Fix routing on nginx
    
    * Fix routing on nginx
    
    * Fix routing on nginx
    
    * Improve Download component
    
    * Create docker dir
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Update backend CI workflow
    
    * Add DJANGO_MODULES_SETTINGS env var to Dockerfile
    
    * Add DJANGO_MODULES_SETTINGS env var to Dockerfile
    
    * Change debug True
    
    * Add csrf trusted origins for backend.robsonbot.com
    
    * Turn back gunicorn
    
    * Fix django static files
    
    * Fix django static files
    
    * Add prod endpoint to AuthContext
    
    * Add prod endpoint to AuthContext
    
    * Improve INSTALL documentation
    
    * fix(installation): Correct dependency installation procedure in the development environment
    
    * feat(django): Update Django to version 5.0.6
    
    * feat(django): Update Django to version 5.0.6
    
    * feat(data-modeling): Implement data modeling for graph patterns
    
    * feat(data-modeling): Implement data modeling for graph patterns
    
    * feat(data-modeling): Implement data modeling for graph patterns
    
    * feat(data-modeling): Implement data modeling for graph patterns
    
    * refactor (workflow): remove outdated CI/CD workflow
    
    * feat: Add Kubernetes manifests for CI/CD pipeline
    
    * feat: Add StochasticOscillator model
    
    * fix: Remove from default settings on manage.py
    
    * clean: Delete Note model migration
    
    * feat: Add some configs and rules

--- backends/monolith/api/models.py
+++ backends/monolith/api/models.py
@@ -42,33 +42,224 @@ class Operation(models.Model):
     stop_gain_percent = models.IntegerField(blank=True)
     stop_loss_percent = models.IntegerField(blank=True)
 
+# The position size for  each trade is automatically
+# calculated based on the  second technical event
+# on the 15-minute chart stop loss.
+
+# Principles
+
+class OddsYourFavor(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    name = models.CharField(max_length=255)
+    experience = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class LimitLosses(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    name = models.CharField(max_length=255)
+    experience = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# Attributes
+
+class Attribute(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    name = models.CharField(max_length=255)
+    experience = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def context(self):
+        return self.name
+
+    def primary_implication(self):
+        return self.name
+
+    def underlying_objective(self):
+        return self.name
+
+    def volume(self):
+        return self.name
+
+    def perspective(self):
+        return self.name
+
 # Technica Analysis
 
-class TechnicalEvent():
+class TechnicalAnalysisInterpretation(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    name = models.CharField(max_length=255)
+    experience = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class TechnicalEvent(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    interpretation = models.ForeignKey(
+        TechnicalAnalysisInterpretation, blank=True, null=True, on_delete=models.SET_NULL)
     type = ('Bullish', 'Bearish')
 
-class Argument():
+class Argument(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    technical_event = models.ForeignKey(
+        TechnicalEvent, blank=True, null=True, on_delete=models.SET_NULL)
     type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Accumulation():
+class Reaseon(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
     type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Breakout():
-    type = ('Bullish', 'Bearish')
+# Facts
 
-class Uptrend():
+class Resistance(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
     type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Downtrend():
+class Support(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
     type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Sideways():
+class Line(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
     type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class TrendLine(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Channel(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Accumulation(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Sideways(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Breakout(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Uptrend(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Downtrend(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    argument = models.ForeignKey(
+        Argument, blank=True, null=True, on_delete=models.SET_NULL)
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 # Chart Patterns
 
-class Rectangle():
+class Rectangle(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
     type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Triangle():
     type = ('Bullish', 'Bearish')
@@ -101,6 +292,11 @@ class MorningStar():
 class EveningStar():
     thre_candles = ('Closure', 'Opening', 'Closure')
     type = ('Bullish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tip = models.TextField(default='Observe evening star on the 5-minute chart. The Evening Star pattern is a reversal pattern that forms when a bearish candlestick is followed by two bullish candlesticks, with the second bullish candlestick closing higher than the first bullish candlestick.')
+
 
 # Statistical Indicators
 
@@ -118,3 +314,55 @@ class BollingerBands():
 
 class StochasticOscillator():
         type = ('Bullish', 'Bearish')
+
+# Rules
+
+class OnePercentOfCapital(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class JustBet4percent(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# Config
+
+class OnlyTradeReversal(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    type = ('Bullish', 'Bearish')
+    description = models.TextField(default = 'Reversals reinforce the trend of the opposing technical event within the chart pattern.')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class MaxTradePerDay(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# Reports
+
+class AlocatedCapitalPercent(models.Model):
+    client = models.ForeignKey(
+        Client, blank=True, null=True, on_delete=models.SET_NULL)
+    tenant_id = 'client_id'
+    type = ('Bullish', 'Bearish')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
