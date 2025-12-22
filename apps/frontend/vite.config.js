396b1f2a0efb2b48dcf2f2d2b0e5931acd5bce44
root <root@ENF-2BG3294.localdomain>
2025-12-22 08:41:43 -0300

    fix(frontend): set vitest root

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(() => {
   return {
+    root: rootDir,
     build: {
       outDir: 'build',
+    },
+    test: {
+      environment: 'jsdom',
     },
     plugins: [react()],
   };
